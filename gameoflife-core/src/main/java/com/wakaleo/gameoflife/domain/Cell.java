52465f51e43b19865c10a3cb328347789cb887de
tsukimi2 <shigotoganbatte@gmail.com>
2018-02-08 06:28:00 +0000

    test

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
  /**
- * Wakaleo Consulting - John Ferguson Smart3
+ * Wakaleo Consulting - John Ferguson Smart4
  * Game of life, demonstration application for Jenkins: The Definitive Guide
  *
  * Cell.java
